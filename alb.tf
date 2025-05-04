135290a38ce0f7e25d3933d4b3a4662bcddda75f
mozcreate-user <moses@medstatlog.com>
2025-05-04 19:29:10 -0400

    changes in load balancer and bucket

--- alb.tf
+++ alb.tf
@@ -1,46 +1,46 @@
 // target group for ALB
 resource "aws_lb_target_group" "tg1" {
-  name        = "alb-tg"
-  port        = 80
-  protocol    = "HTTP"
+  name = "alb-tg"
+  port = 80
+  protocol = "HTTP"
   target_type = "instance"
-  vpc_id      = module.vpc.vpc_id
+  vpc_id = module.vpc.vpc_id
 
   health_check {
-    enabled             = true
-    healthy_threshold   = 3
-    interval            = 10
-    matcher             = 200
-    path                = "/"
-    port                = "traffic-port"
-    protocol            = "HTTP"
-    timeout             = 6
+    enabled = true
+    healthy_threshold = 3
+    interval = 10
+    matcher = 200
+    path = "/"
+    port = "traffic-port"
+    protocol = "HTTP"
+    timeout = 6
     unhealthy_threshold = 3
   }
-  depends_on = [module.vpc]
+  depends_on = [ module.vpc ]
 }
 // attach instance to target group
 
 resource "aws_lb_target_group_attachment" "tga1" {
   target_group_arn = aws_lb_target_group.tg1.arn
-  target_id        = aws_instance.server1.id
-  port             = 80
+  target_id = aws_instance.server1.id
+  port = 80
 }
 
 resource "aws_lb_target_group_attachment" "tga2" {
   target_group_arn = aws_lb_target_group.tg1.arn
-  target_id        = aws_instance.server2.id
-  port             = 80
+  target_id = aws_instance.server2.id
+  port = 80
 }
 
 # Create ALB
 
 resource "aws_lb" "alb1" {
-  name                       = "alb-lb"
-  internal                   = false
-  load_balancer_type         = "application"
-  security_groups            = [aws_security_group.sg2.id]
-  subnets                    = module.vpc.public_subnets
+  name = "alb-lb"
+  internal = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.sg2.id]
+  subnets = module.vpc.public_subnets
   enable_deletion_protection = false
 
 
@@ -50,10 +50,11 @@ resource "aws_lb" "alb1" {
 
 resource "aws_lb_listener" "list1" {
   load_balancer_arn = aws_lb.alb1.arn
-  port              = 80
-  protocol          = "HTTP"
+  port = 80
+  protocol = "HTTP"
   default_action {
-    type             = "forward"
+    type = "forward"
     target_group_arn = aws_lb_target_group.tg1.arn
   }
+  
 }
